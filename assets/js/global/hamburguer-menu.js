d5cbd78dded89e39d13a6d6729ccade98b1d5ffa
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-04 20:05:44 -0300

    revert(frontend): remove the old html files

--- assets/js/global/hamburguer-menu.js
+++
@@ -1,9 +0,0 @@
-function changeMenu(){
-    const menu = document.querySelector('#menu-hamburguer')
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-      } else {
-        menu.style.display = "flex";
-      }
-}
-
