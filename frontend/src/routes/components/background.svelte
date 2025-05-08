4e4a32da35792e247f333896f94f9f2666a291f6
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-07 23:44:44 -0300

    revert(frontend): removes some files that are useless to the project

--- frontend/src/routes/components/background.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Metronome from './metronome.svelte';
-	import Playlist from './playlist.svelte';
-</script>
-
-<div class="bottom-0 left-0 top-0 z-0 flex h-screen bg-zinc-900 pt-10 ring-0">
-	<Playlist />
-	<Metronome />
-</div>
