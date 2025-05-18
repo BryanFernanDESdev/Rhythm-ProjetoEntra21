52debe29234e0db5e559251816d8b420005031f1
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-17 22:26:19 -0300

    feat(backend): adds the controller files

---
+++ backend/src/controllers/tons.controller.js
@@ -0,0 +1,28 @@
+import { where } from "sequelize";
+import models from "../models/index.js"
+
+export const getTom = async (req, res) => {
+    try {
+        const data = await models.Tom.findAll();
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(401).send(error)
+    }
+}
+export const getTomById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const data = await models.Tom.findAll({ where: { id } })
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(401).send(error)
+    }
+}
+export const createTom = async (req, res) => {
+    try {
+        const data = await models.Tom.create(req.params.body)
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(401).send(error)
+    }
+}
