bfce75fc76c86cda06a03a6a1c1098237a68198a
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-18 23:25:08 -0300

    feat(backend): adds the postman test json and changes the main files that are related to the api

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import tonsRoutes from './tons.routes.js';
+import playlistRoutes from './playlists.routes.js'
+import artistaRoutes from './artistas.routes.js'
 
 const router = Router();
 
 router.use('/tom', tonsRoutes);
+router.use('/playlist', playlistRoutes);
+router.use('/artista', artistaRoutes);
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {json} from 'express';
 import cors from 'cors';
 import { initialize } from './config/db.js';
 import router from './routes/index.js'
@@ -7,12 +7,14 @@ initialize()
 
 const port = process.env.SERVER_PORT;
 const app = express();
+app.use(json())
 app.use(cors());
 
 app.use(express.json())
 app.use(router)
 
 app.get('/', (req, res) => {
+    res.send({msg:'servidor Rodando'})
 })
 
 app.listen(port, () => {
