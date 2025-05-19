f7322af6aefbd9e7a61ffe6ae762d24be9addc46
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-18 23:23:27 -0300

    feat(backend): adds the controllers and the routes for the playlist table

--- backend/src/controllers/playlists.controller.js
+++ backend/src/controllers/playlists.controller.js
@@ -1,1 +1,78 @@
+import { where } from "sequelize";
 import models from "../models/index.js"
+
+export const getPlaylist = async (req, res) => {
+    try {
+        const data = await models.Playlist.findAll();
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(500).send({ error })
+    }
+}
+
+export const getPlaylistById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const data = await models.Playlist.findAll({
+            where: {
+                id
+            }
+        });
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(500).send({ error })
+    }
+}
+
+export const createPlaylist = async (req, res) => {
+    try {
+        const { nome, usuario_id } = req.body;
+
+        if (!nome || !usuario_id) {
+            return res.status(400).json({ error: 'Nome e usuario_id são obrigatórios' });
+        }
+
+        const data = await models.Playlist.create({
+            nome,
+            usuario_id,
+        });
+
+        res.status(201).json(data)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao criar playlist' })
+    }
+}
+
+export const updatePlaylist = async (req, res) => {
+    try {
+        const id = req.params.id
+        const {nome} = req.body
+
+        if (!id || !nome) {
+            return res.status(400).json({ error: 'Nome e id são obrigatórios' });
+        }
+
+        const data = await models.Playlist.update({ nome: nome }, { where: { id: id } })
+        res.status(201).json(data)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao alterar a playlist' })
+    }
+}
+
+export const deletePlaylist = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        if (!id ) {
+            return res.status(400).json({ error: 'são obrigatórios' });
+        }
+
+        const data = await models.Playlist.destroy({ where: { id: id } })
+        res.status(201).json(data)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao excluir a playlist' })
+    }
+}
--- backend/src/models/playlists.model.js
+++ backend/src/models/playlists.model.js
@@ -1,8 +1,21 @@
 export default function (sequelize, DataTypes) {
   const Playlist = sequelize.define('Playlist', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     nome: {
       type: DataTypes.STRING(255),
       allowNull: false
+    },
+    usuario_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'usuarios',
+        key: 'id'
+      }
     },
     data_criacao: {
       type: DataTypes.DATE,
@@ -14,6 +27,10 @@ export default function (sequelize, DataTypes) {
   });
 
   Playlist.associate = (models) => {
+    Playlist.belongsTo(models.Usuario, {
+      foreignKey: 'usuario_id'
+    });
+
     Playlist.belongsToMany(models.Musica, {
       through: models.PlaylistMusica,
       foreignKey: 'playlist_id',
