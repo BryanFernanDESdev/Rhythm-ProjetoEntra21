3817e5df4718f4cfb1b33377c2628e3a1df6eb77
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-21 23:05:54 -0300

    feat(backend): adds the new routes for the views

--- backend/src/controllers/views.controller.js
+++ backend/src/controllers/views.controller.js
@@ -2,7 +2,19 @@ import { sequelize } from "../config/config.js"
 
 export const getPlaylist = async (req, res) => {
     try {
-        const data = await sequelize.query('SELECT * FROM view_musicas_playlists_completas;')
+        const data = await sequelize.query('SELECT * FROM view_playlist_detalhada;')
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).send({ error })
+    }
+}
+export const getPlaylistById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await sequelize.query(`SELECT * FROM view_playlist_detalhada where playlist_id=?;`, {
+            replacements: [id]
+        });
+
         res.status(200).json(data)
     } catch (error) {
         res.status(500).send({ error })
--- backend/src/routes/views.routes.js
+++ backend/src/routes/views.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getArtists, getPlaylist } from "../controllers/views.controller.js";
+import { getArtists, getPlaylist, getPlaylistById } from "../controllers/views.controller.js";
 const router = Router()
 
 router.get('/playlist', getPlaylist)
+router.get('/playlist/:id', getPlaylistById)
 router.get('/artist', getArtists)
 
 export default router
